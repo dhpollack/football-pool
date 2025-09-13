144214cb5d4b6fb2df499e51ae9a8b0d2fec2b25
David Pollack <david@da3.net>
2025-09-13 22:16:54 +0200

    chore: chuck frontend app (#13)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,4 +8,21 @@ export default defineConfig({
   define: {
     global: {},
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-router-dom"],
+          ui: [
+            "@mui/material",
+            "@mui/icons-material",
+            "@emotion/react",
+            "@emotion/styled",
+          ],
+          data: ["@tanstack/react-query", "axios"],
+          auth: ["react-auth-kit"],
+        },
+      },
+    },
+  },
 });
